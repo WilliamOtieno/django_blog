acca8c5de6545b56b5fdd789f22208ebdb7faba7
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-28 22:35:31 +0300

    minor

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h2>Blog About</h2>')

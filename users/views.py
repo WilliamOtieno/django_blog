568b14832b2d63cf6df7af62155b5ae50197632c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-29 12:59:29 +0300

    added decorator

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -10,7 +11,6 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f"Account created successfully! You can now log in.")
             return redirect('login')
     else:
@@ -18,5 +18,6 @@ def register(request):
     return render(request, 'users/register.html', context={'form': form})
 
 
+@login_required
 def profile(request):
     return render(request, template_name='users/profile.html')

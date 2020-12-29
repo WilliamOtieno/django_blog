b344ef2e10057b9db082674fb5252cbad64c7a31
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-29 08:35:07 +0300

    registered model

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
-posts = [
-    {
-        'author': 'William JO',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': '28th Dec 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': '29th Dec 2020'
-    }
-]
-
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, template_name='blog/home.html', context=context)
 

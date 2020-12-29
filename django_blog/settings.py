7e253d598e3e1721c74f85f6b474447136378cf9
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-29 12:51:21 +0300

    added profile

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -124,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
--- users/views.py
+++ users/views.py
@@ -11,8 +11,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username}!")
-            return redirect('blog-home')
+            messages.success(request, f"Account created successfully! You can now log in.")
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', context={'form': form})
+
+
+def profile(request):
+    return render(request, template_name='users/profile.html')

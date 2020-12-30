79bb70a16dd1153cbeceb72e3539f1484e3f120c
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-30 22:37:51 +0300

    class based views

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.views.generic import ListView, DetailView, CreateView
 
 # Create your views here.
 
-def home(request):
+"""def home(request):
     context = {
         'posts': Post.objects.all()
     }
-    return render(request, template_name='blog/home.html', context=context)
+    return render(request, template_name='blog/home.html', context=context)"""
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 
 def about(request):

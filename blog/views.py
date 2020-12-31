82ac41d8b1beba10842be6f48b02723637fc51c8
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-31 11:24:43 +0300

    more pagination

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+
 
 # Create your views here.
-
-"""def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, template_name='blog/home.html', context=context)"""
 
 
 class PostListView(ListView):
@@ -17,7 +13,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 2
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):

422687d86c258eda65f3a95e9e04169bafad8de8
WilliamOtieno <jimmywilliamotieno@gmail.com>
2020-12-31 16:06:12 +0300

    abstraction

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jimmywilliamotieno@gmail.com'
 EMAIL_HOST_PASSWORD = 'drqurkhpecxbhmhn'
+
+
+django_heroku.settings(locals())
